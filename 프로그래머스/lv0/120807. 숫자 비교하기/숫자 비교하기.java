321ec6eacef872641a682953e531db6fc918c4b5
smarto <145187622+arbiterkdh@users.noreply.github.com>
2023-09-20 20:52:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int i, int x) {
+        int answer;
+        if(i==x){
+            answer=1;
+    }else answer=-1;
+                 
+        return answer;
+    }
+}
