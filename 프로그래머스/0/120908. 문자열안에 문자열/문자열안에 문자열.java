b2543158c14dcccd9f3a7310938e102336dc4f21
smarto <145187622+arbiterkdh@users.noreply.github.com>
2024-02-23 12:30:45 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,14 +1,7 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        boolean result = str1.contains(str2);
-        
-        
-        if(result) {
-            answer = 1;
-        }else {
-            answer =2;
-        }
+         boolean result = str1.contains(str2);
+            int answer = result? 1 : 2;
 
         return answer;
     }
