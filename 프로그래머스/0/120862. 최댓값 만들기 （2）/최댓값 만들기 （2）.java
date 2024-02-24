5dcb89192ebd17db01efceb49917ad0d922f18fb
smarto <145187622+arbiterkdh@users.noreply.github.com>
2024-02-24 16:09:28 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.32 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,36 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int max = -10_000;
-        int subMax = -10_000;
-        int answerplus = 0;
-        int answerminus = 0;
-        
-        for(int i=0; i<numbers.length;i++){
-            if(numbers[i]>max){
-                subMax = max;
-                max = numbers[i];
-            }else if(numbers[i]>subMax){
-                subMax = numbers[i];
-            }
-        }
-        answerplus = max*subMax;
-        
-        for(int i=0; i<numbers.length;i++){
-            if(numbers[i]<max){
-                subMax = max;
-                max = numbers[i];
-            }else if(numbers[i]<subMax){
-                subMax = numbers[i];
-            }
-        }
-        answerminus = max*subMax;
-        
-        if(answerplus>answerminus){
-            answer=answerplus;
-        }else{
-            answer=answerminus;
-        }
+        Arrays.sort(numbers);
+        int answer1 = numbers[0]*numbers[1];
+        int answer2 = numbers[numbers.length-2]*numbers[numbers.length-1];
+
+        int answer =(answer1>answer2) ? answer1 : answer2 ;
         return answer;
     }
 }
