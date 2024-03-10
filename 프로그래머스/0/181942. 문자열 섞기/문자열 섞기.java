483b32b1adf37f920dcf9227b7d53a8a5e3f3dcb
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-10 22:21:34 +0900

    [level 0] Title: 문자열 섞기, Time: 0.16 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,10 +2,8 @@ class Solution {
     public String solution(String str1, String str2) {
         StringBuilder sb = new StringBuilder();
         String[] a = str1.split("") , b = str2.split("");
-        String answer = "";
         for(int i = 0; i<str1.length();i++){
-            sb.append(a[i]);
-            sb.append(b[i]);
+            sb.append(a[i]).append(b[i]);
         }
         return sb.toString();
     }
