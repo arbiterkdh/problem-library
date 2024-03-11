bea51daa9df237c04414f9e1e94b5eadcbf68262
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-12 00:42:13 +0900

    [level 0] Title: 배열 만들기 6, Time: 17.92 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0;i<arr.length;i++){
+            if(list.isEmpty()){
+                list.add(arr[i]);
+            }else if(list.get(list.size()-1)==arr[i]){
+                list.remove(list.size()-1);
+            }else{
+                list.add(arr[i]);
+            }
+        }
+        
+        if(list.isEmpty()){
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i = 0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
