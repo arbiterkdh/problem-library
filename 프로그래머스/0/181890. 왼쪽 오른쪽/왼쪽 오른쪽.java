745304f7982183a87d8754b93a705d202135586e
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-12 01:06:49 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+        
+        for(int i = 0; i<str_list.length; i++){
+            
+            if(str_list[i].equals("l")){
+                answer = new String[i];
+                
+                for(int j = 0; j<i; j++){
+                    answer[j] = str_list[j];
+                }
+                
+                return answer;
+                
+            }else if(str_list[i].equals("r")){
+                answer = new String[str_list.length-i-1];
+                
+                for(int j = 0; j<str_list.length-i-1;j++){
+                    answer[j] = str_list[i+1+j];
+                }
+                
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
