37472aecad11f47e21405f7ab9b3134b2703af57
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-11 15:04:32 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.65 ms, Memory: 95.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public Object[] solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i<intStrs.length ; i++){
+            if(k < Integer.parseInt(intStrs[i].substring(s,s+l)) ){
+                list.add(Integer.parseInt(intStrs[i].substring(s,s+l)));
+            }
+        }
+        return list.toArray();
+    }
+}
