fca4c019a61119c1179f7797c15677754a622f51
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-11 16:32:27 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        // V 는 or 연산자, A 는 and 연산자.
+        return ( x1 || x2 )&&( x3 || x4 );
+    }
+}
