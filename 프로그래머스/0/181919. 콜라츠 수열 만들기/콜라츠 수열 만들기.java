0778160598f4f05d9a95e2b0b81942673c199daa
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-11 11:19:55 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static ArrayList<Integer> list = new ArrayList<>();
+    
+    public Object[] solution(int n) {
+        
+        list.add(colatz(n));
+        return list.toArray();
+    }
+    
+    
+    
+    
+    public static int colatz (int n){
+        list.add(n);
+        if(n%2==1){
+            n = 3*n+1;
+        }else{
+            n /= 2;
+        }
+        return n==1 ? n : colatz(n);
+    }
+}
