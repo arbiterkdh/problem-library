267b3cf3e8c4434ac1973c193a0da701c58fdf4c
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-11 14:11:08 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.51 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for(int[] e : queries){
             for(int i = e[0];i<=e[1];i++){
-                arr[i] = arr[i]>=e[0] || arr[i]<=e[1] ? arr[i]+1 : arr[i];
+                arr[i]++;
             }
         }
         return arr;
