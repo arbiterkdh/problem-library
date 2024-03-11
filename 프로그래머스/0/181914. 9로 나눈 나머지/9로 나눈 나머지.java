365b1ea1e85d8c152c0ab47e6749ebbf15021287
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-11 12:32:22 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String number) {
         int answer = 0;
         for(int i =0;i<number.length();i++){
-            answer += Integer.parseInt(number.substring(i,i+1));
+            answer += number.charAt(i)-'0';
         }
         return answer%9;
     }
