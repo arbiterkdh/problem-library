6ae439e3bc2d058bef5844d55a53d6eb75747864
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-13 21:05:20 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int e: arr){
+            answer+=e;
+        }
+        return answer/arr.length;
+    }
+}
