49be334af10278ce829993022a949565154bca23
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-13 17:10:17 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 3.97 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (x,y) -> x.charAt(n)==y.charAt(n) ? x.compareTo(y) : x.charAt(n) - y.charAt(n));
+        return strings;
+    }
+}
