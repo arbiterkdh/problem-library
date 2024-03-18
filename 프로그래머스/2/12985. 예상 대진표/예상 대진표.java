5189a7d62693e1cd2f41692b8353c39e8828ed8a
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-18 21:02:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        
+        while(Math.abs(a-b)!=0){
+            a = a%2==0 ? a/2 : a/2+1;
+            b = b%2==0 ? b/2 : b/2+1;
+            answer++;
+        }
+
+        return answer-1;
+    }
+}
