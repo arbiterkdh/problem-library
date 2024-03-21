0e91f720ea817b2ac47759b3181ed6b007d702fd
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-21 19:42:07 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,33 +1,23 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] nums) {
-        Arrays.sort(nums);
-        int range = nums[nums.length-1]+nums[nums.length-2]+nums[nums.length-3];
-        
-        boolean[] primeNumbers = new boolean[range+1];
-        
-        for(int i=7; i<=range; i++){
-            int j=2;
-            while(j<=i/2){
-                if(i%j==0) break;
-                j++;
-            }
-            if(j>i/2){
-                primeNumbers[i] = true;
-            }
-        }
-        
         int cnt = 0;
-        for(int i=0; i<nums.length-2; i++){
-            for(int j=i+1; j<nums.length-1; j++){
-                for(int k=j+1; k<nums.length; k++){
-                    if(primeNumbers[nums[i]+nums[j]+nums[k]]) cnt++;
+
+        for(int i = 0; i<nums.length-2; i++){
+            for(int j = i+1; j<nums.length-1; j++){
+                for(int k = j+1; k<nums.length; k++){
+                    if(isPrime(nums[i]+nums[j]+nums[k])) cnt++;
                 }
             }
         }
-        
 
         return cnt;
+    }
+    
+    public static boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i<=Math.sqrt(num); i++){
+            if(num%i==0) cnt++;
+        }
+        return cnt == 1;
     }
 }
