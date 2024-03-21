fdd41ae513e6b14764d19ac625ad3fa3e3901de7
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-21 20:39:46 +0900

    [level 1] Title: 덧칠하기, Time: 1.35 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,14 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        boolean[] paint = new boolean[n+1];
-        for(int s : section){
-            paint[s] = true;
-        }
-        int cnt = 0;
-        for(int sector = 1; sector<=n; sector++){
-            if(paint[sector]){
+        int roller = section[0]+m-1;
+        int cnt = 1;
+        
+        for(int i = 1; i<section.length; i++){
+            if(section[i]>roller){
+                roller = section[i]+m-1;
                 cnt++;
-                sector+=m-1;
             }
         }
-        
         return cnt;
     }
 }
