6ffb330e84d2b5c10649eb44689f9bfcd57af4ed
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-25 08:46:44 +0900

    [level 0] Title: 배열의 평균값, Time: 1.89 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,11 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        double x=0;
-        for(int i=0;i<numbers.length;i++){
-            x+=numbers[i];
-        }
-        answer=x/numbers.length;
-        return answer;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
