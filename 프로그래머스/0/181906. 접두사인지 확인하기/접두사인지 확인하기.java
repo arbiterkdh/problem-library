cbd65ab7475dbd72726867263643012ee8438f66
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-25 12:00:08 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 7.96 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,5 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        return my_string.indexOf(is_prefix)==0 ? 1 : 0;
+        return IntStream.rangeClosed(0, my_string.length())
+            .mapToObj(i -> my_string.substring(0, i))
+            .anyMatch(s -> s.equals(is_prefix)) ? 1 : 0;
     }
 }
