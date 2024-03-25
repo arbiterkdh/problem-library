fbdb296d391b55edace7470598c235e0678b5264
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-25 09:06:21 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,26 +1,26 @@
 class Solution {
     public int solution(String[] babbling) {
-        int answer = 0;
-        String[] words = {"aya","ye","woo","ma"};
-        //각 babbling[] 원소에  각 words[]를 contains 확인 후, equal 확인, 
-        //true면 result ++, false 면 다음 words[] 확인, 반복해서 4개 다 확인 후 equal 있으면 result++
-        //일단 길이 10 넘으면 제외.
-        //
-        int run = 0;
-        while(run<4){
-            for(int i = 0;i<babbling.length;i++){
-                for(int j = 0; j<words.length;j++){
-                    if(babbling[i].length()<=10 && babbling[i].contains(words[j])){
-                        babbling[i] = babbling[i].replace(words[j],"+");
-                    }
-                }
-            }
-            run++;
-        }
-        for(int i =0;i<babbling.length;i++){
-            if(babbling[i].equals("+")||babbling[i].equals("++")||babbling[i].equals("+++")||babbling[i].equals("++++")){answer++;}
-        }
+        int cnt = 0;
+        int len = babbling.length;
         
-        return answer;
+        for (int i = 0; i < len; i++) {
+            String s = babbling[i];
+            
+            if (s.contains("aya")) {
+                s = s.replace("aya"," ");
+            } 
+            if (s.contains("ye")) {
+                s = s.replace("ye"," ");
+            } 
+            if (s.contains("woo")) {
+                s = s.replace("woo"," ");
+            } 
+            if (s.contains("ma")) {
+                s = s.replace("ma"," ");
+            } 
+            s = s.replace(" ","");
+            if (s.isEmpty()) cnt++; 
+        }
+        return cnt;
     }
 }
