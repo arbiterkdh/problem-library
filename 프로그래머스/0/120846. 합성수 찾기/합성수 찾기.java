d45fb027b4d110b284dc7a69c5899df7d9be45e5
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-25 09:58:11 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        for(int i = n; i>3; i--){
-            for(int j = 2; j<n; j++){
-                if(i%j==0 && i!=j){
-                    
+        for (int i = 4; i <= n; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
                     answer++;
                     break;
                 }
