4b8d9d0d4c86673e4df4d760eafc5a7f1d598cd7
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-27 21:21:59 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.29 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,31 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        int cnt = 0;
-        long temp = n;
-        long temp2 = 1;
-        while(temp!=0){
-            temp/=10;
-            temp2*=10;
-            cnt++;
+        String[] s = Long.toString(n).split("");
+        Arrays.sort(s);
+        StringBuilder sb = new StringBuilder();
+        for (String c : s) {
+            sb.append(c);
         }
-        temp2/=10;
-        long[] arr = new long[cnt];
-        for(int i = 0; i<arr.length; i++){
-            arr[i] = (long)n%10;
-            n/=10;
-        }
-        for(int i = 0; i<arr.length; i++){
-            for(int j= i+1; j<arr.length;j++){
-                if(arr[i]<arr[j]){
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-            answer+= temp2*arr[i];
-            temp2/=10;
-        }
-        return answer;
+        return Long.parseLong(sb.reverse().toString());
     }
 }
