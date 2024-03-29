2e2fecfc1f95b1dd4061b74371e95c24b02af9a5
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-29 12:16:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        if(s.length()==4||s.length()==6){
-            s = s.replaceAll("[0-9]","");
-            if(s.isEmpty()) return true;
-        }
-        return false;
+        return (s.length() == 4 || s.length() == 6 ) && s.matches("[0-9]+");
     }
 }
