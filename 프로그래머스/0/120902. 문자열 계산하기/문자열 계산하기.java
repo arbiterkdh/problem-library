27f970890dafbe582ea49fad0ecbaf15bbe382bd
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-30 00:00:54 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(String my_string) {
-        String[] strArr = my_string.split(" ");
-        int answer = Integer.parseInt(strArr[0]);
-        for(int i = 0;i<strArr.length-2;i+=2){
-            if(strArr[i+1].equals("+")){
-                answer += Integer.parseInt(strArr[i+2]);
-            }else{answer -= Integer.parseInt(strArr[i+2]);}
+        String[] arr = my_string.split(" ");
+        int answer = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i].equals("+")) {
+                answer += Integer.parseInt(arr[i + 1]);
+            } else if (arr[i].equals("-")) {
+                answer -= Integer.parseInt(arr[i + 1]);
+            }
         }
         return answer;
     }
