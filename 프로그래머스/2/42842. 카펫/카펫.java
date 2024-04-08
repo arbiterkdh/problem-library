1397690bd1dab85e468fd6ef0e169862088aa34b
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-08 15:41:41 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            if (yellow % i == 0) {
+                if (brown == 2 * (i + yellow / i + 2)) {
+                    answer[0] = yellow / i + 2;
+                    answer[1] = i + 2;
+                }
+            }
+        }
+        return answer;
+    }
+}
