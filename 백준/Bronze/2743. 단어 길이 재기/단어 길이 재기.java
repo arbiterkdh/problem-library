87a8967baa2736f4c9de9cf1f00822180c7ea3a8
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-18 10:41:51 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        bw.write(s.length() + "");
+        bw.flush();
+        bw.close();
+    }
+}
