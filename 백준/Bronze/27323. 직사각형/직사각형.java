2f82a56f784e406e008c386262997ac1e8d3e7f9
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-22 16:49:22 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        br.close();
+        bw.write((A * B) + "");
+        bw.flush();
+        bw.close();
+    }
+}
