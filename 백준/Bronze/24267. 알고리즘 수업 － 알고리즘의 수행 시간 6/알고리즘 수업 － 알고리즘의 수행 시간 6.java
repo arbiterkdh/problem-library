1a91711e50511f5731a4c72b2c8e77ebbff970c2
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-22 21:18:51 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 144 ms, Memory: 14660 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long n = Long.parseLong(br.readLine());
+        br.close();
+        bw.write((n * (n - 1) * (n - 2) / 6) + "\n" + "3");
+        bw.flush();
+        bw.close();
+    }
+}
