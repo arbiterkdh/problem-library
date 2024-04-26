373792eb5a6150ed584aeda801d242e5ef1de6b5
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-26 11:04:19 +0900

    [Bronze II] Title: 대표값2, Time: 140 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        br.close();
+        bw.write(sum / 5 + "\n");
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                if (arr[i] > arr[j]) {
+                    int swap = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = swap;
+                }
+            }
+        }
+        bw.write(arr[2] + "");
+        bw.flush();
+        bw.close();
+        
+    }
+}
