d34f0a53814b188b962ddb36fd5974e2fe95f320
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-04-28 23:37:46 +0900

    [Silver V] Title: 창문 닫기, Time: 132 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        bw.write((int) Math.sqrt(N) + "");
+        bw.flush();
+        bw.close();
+    }
+}
