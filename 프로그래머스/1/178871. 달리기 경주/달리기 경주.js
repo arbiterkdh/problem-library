8a016ef1c7f5a025cfbb63c6a5329eede6172c2b
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2025-11-28 16:35:22 +0900

    [level 1] Title: 달리기 경주, Time: 231.17 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,28 +1,21 @@
 function solution(players, callings) {
-    let ranking = [];
     let len = players.length;
     let nameKeyRanking = new Map();
-    let numberKeyRanking = new Map();
     
     for (let rank = 0; rank < len; rank++) {
         nameKeyRanking.set(players[rank], rank);
-        numberKeyRanking.set(rank, players[rank]);
     }
     
     let behindNum = 0;
     let aheadName = "";
     for (let behindName of callings) {
         behindNum = nameKeyRanking.get(behindName);
-        aheadName = numberKeyRanking.get(behindNum-1);
+        aheadName = players[behindNum-1];
         nameKeyRanking.set(behindName, behindNum-1);
         nameKeyRanking.set(aheadName, behindNum);
-        numberKeyRanking.set(behindNum-1, behindName);
-        numberKeyRanking.set(behindNum, aheadName);
+        players[behindNum-1] = behindName;
+        players[behindNum] = aheadName;
     }
     
-    for (let rank = 0; rank < len; rank++) {
-        ranking.push(numberKeyRanking.get(rank));
-    }
-    
-    return ranking;
+    return players;
 }
