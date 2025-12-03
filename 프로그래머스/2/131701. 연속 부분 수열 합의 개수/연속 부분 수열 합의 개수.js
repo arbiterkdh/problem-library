41fab52bc7bf2fa07e13a6b6049ada4e5b7b5a8e
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2025-12-04 00:52:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 79.25 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,22 +1,36 @@
 function solution(elements) {
-    let cases = new Set(elements);
-    cases.add(elements.reduce((acc, curr) => acc + curr, 0));
+    let cases = new Set();
+    let circular = elements.concat(elements);
     
-    let caseLen = 2;
-    let tmpElements = [];
-    let addNum = 0;
-    
-    while (caseLen < elements.length) {
-        tmpElements = [...elements, ...elements.slice(0, caseLen)];
-        for (let i = 0; i < elements.length; i++) {
-            for (let j = i, k = 0; k < caseLen; j++, k++) {
-                addNum += tmpElements[j];
-            }
-            cases.add(addNum);
-            addNum = 0;
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            cases.add(sum);
         }
-        caseLen++;
     }
-    
-    return cases.size;
+    return cases.size; // 다른사람의 풀이를 보고 수학을 배웠다...
 }
+
+// function solution(elements) {
+//     let cases = new Set(elements);
+//     cases.add(elements.reduce((acc, curr) => acc + curr, 0));
+    
+//     let caseLen = 2;
+//     let tmpElements = [];
+//     let addNum = 0;
+    
+//     while (caseLen < elements.length) {
+//         tmpElements = [...elements, ...elements.slice(0, caseLen)];
+//         for (let i = 0; i < elements.length; i++) {
+//             for (let j = i, k = 0; k < caseLen; j++, k++) {
+//                 addNum += tmpElements[j];
+//             }
+//             cases.add(addNum);
+//             addNum = 0;
+//         }
+//         caseLen++;
+//     }
+    
+//     return cases.size;
+// }
