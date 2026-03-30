b1ba4bc343e01be0706776e3641472089099ca19
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-03-30 18:07:43 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -3,19 +3,18 @@ SELECT
     GU.USER_ID,
     GU.NICKNAME,
     GU.CITY || ' ' || GU.STREET_ADDRESS1 || ' ' || GU.STREET_ADDRESS2 AS "전체주소",
-    REGEXP_REPLACE(GU.TLNO, '(\d{3})(\d{4})(\d{4})', '\1-\2-\3') AS "전화번호"
+    REGEXP_REPLACE(TLNO, '(\d{3})(\d{4})(\d{4})', '\1-\2-\3') AS "전화번호"
 FROM
-    USED_GOODS_BOARD GB
-JOIN
-    USED_GOODS_USER GU ON GB.WRITER_ID = GU.USER_ID
-GROUP BY
-    GU.USER_ID, 
-    GU.NICKNAME, 
-    GU.CITY, 
-    GU.STREET_ADDRESS1, 
-    GU.STREET_ADDRESS2, 
-    GU.TLNO
-HAVING
-    COUNT(*) >= 3
+    USED_GOODS_USER GU
+JOIN (
+    SELECT
+        WRITER_ID
+    FROM
+        USED_GOODS_BOARD
+    GROUP BY
+        WRITER_ID
+    HAVING
+        COUNT(*) >= 3
+) GB ON GU.USER_ID = GB.WRITER_ID
 ORDER BY
     GU.USER_ID DESC
