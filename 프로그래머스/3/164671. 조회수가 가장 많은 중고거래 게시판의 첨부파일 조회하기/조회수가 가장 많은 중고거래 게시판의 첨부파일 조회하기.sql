64be64f74ec26691298704aa5362f0f80388de5a
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-01 14:20:11 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT
+    '/home/grep/src/' || B.BOARD_ID || '/' || F.FILE_ID || F.FILE_NAME || F.FILE_EXT AS FILE_PATH
+FROM (
+    SELECT
+        BOARD_ID,
+        VIEWS
+    FROM
+        USED_GOODS_BOARD
+    ORDER BY
+        VIEWS DESC
+    FETCH FIRST 1 ROWS ONLY
+) B
+JOIN
+    USED_GOODS_FILE F ON B.BOARD_ID = F.BOARD_ID
+ORDER BY
+    F.FILE_ID DESC
