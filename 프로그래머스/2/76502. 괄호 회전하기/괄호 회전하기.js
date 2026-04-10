43d2a1399797ba0ba35988a6664d76ff0028b2db
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-10 11:45:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 27.15 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,29 +1,29 @@
 function solution(s) {
-    if (s.length % 2 == 1) return 0;
-
+    if(s.length % 2 === 1) return 0;
+    s += s;
     let matchCnt = 0;
-    const sArr = s.split("");
     const parentheses = {
         "(": ")",
         "{": "}",
         "[": "]",
     };
-
-    for (let i = 0; i < sArr.length; i++) {
+    for(let i = 0; i < s.length / 2; i++) {
         const stack = [];
         let flag = true;
-        for (let j = 0; j < sArr.length; j++) {
-            if (sArr[j] in parentheses) {
-                stack.push(sArr[j]);
-            } else if (parentheses[stack.pop()] !== sArr[j]) {
-                flag = false;
+
+        for (let j = i; j < i + s.length / 2; j++) {
+            if (s[j] in parentheses) {
+                stack.push(s[j]);
+            } else if(parentheses[stack.pop()] !== s[j]) {
+                flag = false
                 break;
             }
         }
-        if (flag) {
+
+        if (stack.length == 0 && flag) {
             matchCnt++;
         }
-        sArr.push(sArr.shift());
     }
+
     return matchCnt;
 }
