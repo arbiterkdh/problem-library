a52ac47a1dd05e269b7cdad5f7beb0dc21efa6f0
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-13 16:21:04 +0900

    [level 2] Title: [1차] 캐시, Time: 22.47 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,49 +1,18 @@
-function solution(cacheSize, cities) {    
-    class LRU {
-        constructor(size) {
-            this.store = {};
-            this.dummys = new Set();
-            this.size = size;
-            this.head = 0;
-            this.tail = 0;
-        }
-        enqueue(value) {
-            this.store[this.tail++] = value;
-            if (this.tail - this.head - this.dummys.size > this.size) {
-                this.dequeue();
-            }
-        }
-        dequeue() {
-            while (this.dummys.has(this.head)) {
-                this.dummys.delete(this.head);
-                this.head++;
-            }
-            delete this.store[this.head++];
-        }
-        findIndex(value) {
-            for (let i = this.head; i < this.tail; i++) {
-                if (value == this.store[i]) return i;
-            }
-            return -1;
-        }
-        use(idx) {
-            const value = this.store[idx];
-            delete this.store[idx];
-            this.dummys.add(idx);
-            this.enqueue(value);
-        }
-    }
+function solution(cacheSize, cities) {
     let timeTaken = 0;
-    const cache = new LRU(cacheSize);
+    const cache = new Map();
     for (let i = 0; i < cities.length; i++) {
         const city = cities[i].toLowerCase();
-        const cityIdx = cache.findIndex(city);
-        if (cityIdx > -1) {
-            cache.use(cityIdx);
-            timeTaken += 1;
+        if (cache.has(city)) {
+            timeTaken++;
+            cache.delete(city);
+            cache.set(city);
         } else {
-            cache.enqueue(city);
             timeTaken += 5;
+            cache.set(city);
+            if (cache.size > cacheSize) {
+                cache.delete(cache.keys().next().value);
+            }
         }
     }
     return timeTaken;
