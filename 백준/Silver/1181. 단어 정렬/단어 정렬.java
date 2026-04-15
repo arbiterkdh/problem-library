4588a0ecb2fbed3a8742575bcb5cf14567a54e66
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-15 15:13:06 +0900

    [Silver V] Title: 단어 정렬, Time: 344 ms, Memory: 23920 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+        String[] arr = set.toArray(new String[0]);
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            }
+            return a.length() - b.length();
+        });
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
